1c3a52f2ea33a855f4d03afbf97bd0ef352549ff
gbhavalkar <gbhavalkar@gmail.com>
2019-02-12 22:46:16 +0530

    Setup actions reducers with redux-starter-kit

--- src/reducers/chessboard.reducer.js
+++ src/reducers/chessboard.reducer.js
@@ -1,3 +1,6 @@
+import { createReducer } from 'redux-starter-kit'
+import * as actions from '../actions/actions'
+
 // Setting the initial state supplied to the chess board
 const initialState = {
 }
@@ -5,11 +8,38 @@ const initialState = {
 /**
  * * Reducer containing state of the chessboard maintaining the properties describing 
  * * the value a position on the chessboard holds 
+ * The createReducer uses "immer" which lets you write code that "mutates" some data, 
+ * but actually applies the updates immutably. 
+ * This makes it effectively impossible to accidentally mutate state in a reducer.
  * TODO: Maintaining an UNDO & REDO history
  */
-export default (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return state;
+const chessBoardReducer = createReducer( initialState, {
+    [actions.addMove] : ( state, action ) => {
+        //Update the Total moves
+        state.totalMoves = state.totalMoves + 1
+
+        //Update the current move
+        state.currentMove = state.totalMoves
+
+        const updatedPositions = action.payload
+        Object.keys( updatedPositions ).map( position => {
+            // Update the chess board positions affected by a players move
+            state.chessBoard[position] = {
+                affectedMove: state.currentMove,
+                currentIndexInPositionsArray: state.chessBoard[position] 
+                                                ? state.chessBoard[position].currentIndexInPositionsArray + 1
+                                                : 0
+            }
+            const newPositionObject = {
+                value: updatedPositions[position].value,
+                house: updatedPositions[position].house
+            }
+            // Add the new object for the position in the positions array 
+            if( state.positions[position] ){
+                state.positions[position].push(newPositionObject)
+            }else{
+                state.positions[position] = [newPositionObject]
+            }
+        })
     }
-};
+})
